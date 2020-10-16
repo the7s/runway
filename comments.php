a697075116cd58844f151318ef8c105baa9228c8
cwang <cwang@iiiview.com>
2020-10-16 10:39:26 +0800

    修改 gitalk 配置项

--- comments.php
+++ comments.php
@@ -16,7 +16,7 @@
         // gitalk的主要参数
         clientID: '0acdd483257d3c78bd54',
 		clientSecret: '78bc5e954f84a4f9d1a8fc6f850489bbc8fd7369',
-		repo: 'https://amoker.com',
+		repo: 'gittalk_repo',
 		owner: 'the7s',
 		admin: ['the7s'],
 		id:window.location.pathname,
